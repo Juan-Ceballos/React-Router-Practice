620967ba786328608f1a70d59d0d612d2eaa7c5a
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-03-02 17:45:54 -0500

    setup introductory routes to what would be a Home and About component using BrowserRouter and assign paths

--- react-router-practice/src/App.jsx
+++ react-router-practice/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter } from 'react-router-dom'
 import ReactDom from 'react-dom/client'
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 root.render(
@@ -16,9 +19,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <p>hello</p>
-    </>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </div>
   )
 }
 
